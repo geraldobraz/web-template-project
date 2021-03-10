b4969e095e2b904c39af5cf075360cebe9839219
Geraldo Braz <gban50@gmail.com>
2021-03-10 20:43:02 -0300

    Updating frontend project

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -9,16 +9,18 @@ export default createGlobalStyle`
   }
   body {
     -webkit-font-smoothing: antialiased;
+    background-color: #fff;
+    color: #000;
   }
   body, input, button {
     font-family: Roboto, sans-serif;
     font-size: 16px;
   }
-  #root {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 40px 20px;
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
   }
+
   button {
     cursor: pointer;
   }
